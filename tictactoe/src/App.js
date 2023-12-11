4121c526438caf395ed8d63663ab1350522b645e
moonira98 <mirzoevamunira98@gmail.com>
2023-12-12 01:07:09 +0500

    Project is done

---
+++ tictactoe/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TicTacToe from './TicTacToe/TicTacToe';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <TicTacToe />
+    </div>
+    
+  );
+}
+
+export default App;
